631c75fb781e9b9a4241e154632d5b492eae9eca
gualX <gutieralbuquerque@gmail.com>
2025-05-19 20:14:52 -0300

    ajustes no tailwind.config

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css"; // Arquivo de estilos globais
+import "./index.css"; // Arquivo de estilos globais
 import "primereact/resources/themes/lara-light-indigo/theme.css"; // Tema do PrimeReact
 import "primereact/resources/primereact.min.css"; // Estilos do PrimeReact
 
